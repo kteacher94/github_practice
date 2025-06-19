9e0d5635e6398b28d0a22212b08ed138d333ade7
st.kong <st.kong@interxlab.com>
2025-06-19 10:17:34 +0900

    refactor: add function type error

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import Union
 
 def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        raise TypeError("x and y must be either int or float")
+    
     return x + y
 
 if __name__ == "__main__":
